9b84a386b211f29ba8f9baa0f9171a274d7b50f2
Marcel Prakash <prakashmarcel@gmail.com>
2026-03-09 14:37:15 +1300

    feat: add sitemap integration and navigation fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://gdnz.org',
   output: 'static',
   adapter: cloudflare(),
+  integrations: [
+    sitemap(),
+  ]
 });
